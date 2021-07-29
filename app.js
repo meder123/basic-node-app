fa22e459b1d7b452e660fd3e2ea7041cd21de0b1
meder <meder420@outlook.com>
2021-07-29 18:00:12 +0100

    stupid typo fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<hi>Hello There.</hi>');
+    res.send('<h1>Hello There.</h1>');
 });
 
 app.listen(5000, () => {
